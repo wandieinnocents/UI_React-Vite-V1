7ac3236dce63ff9ce4a0b9c21c012c55e361cc90
wandieinnocents <wandieinnocent2@gmail.com>
2023-05-26 21:57:12 +0300

    Add component

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      This is about component
+    </div>
+  );
+}
+
+export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      Home
+      This is a home component
     </div>
   );
 }
