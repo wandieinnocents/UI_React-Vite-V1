4f7b2195cc2cd1d1e1217e4b6000e62867d11c50
wandieinnocents <wandieinnocent2@gmail.com>
2023-05-26 15:34:53 +0300

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Introduction</h1>
+      
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Creating User intefaces with react js
       </p>
     </>
   )
