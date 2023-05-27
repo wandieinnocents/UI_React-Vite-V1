4bb4549ab7dbbc71777c1a5b551e7daff071a354
wandieinnocents <wandieinnocent2@gmail.com>
2023-05-27 20:30:06 +0300

    Modify homepae

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ import Stack from '@mui/material/Stack';
 // images
 import banner from "../assets/seo3.png"
 import banner1 from "../assets/seo.png"
-// import banner2 from "../assets/seo3.png"
+import banner2 from "../assets/seo1.png"
 // import banner3 from "../assets/seo3.png"
 
 
@@ -26,9 +26,10 @@ import banner1 from "../assets/seo.png"
 
 export default function Home() {
   return (
-    // landing view 1
+   
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2} style={{ backgroundColor:'black', color:'white' }}>
+    {/* Grid row */}
+      <Grid container spacing={2} style={{ backgroundColor:'black', color:'white', borderBottomLeftRadius : 10, borderBottomRightRadius : 300, }}>
       {/* content */}
         <Grid xs={12} md={6} lg={6} xl={6} 
         
@@ -62,12 +63,12 @@ export default function Home() {
         
       </Grid>
 
-      {/* web development */}
+      {/* Grid row  */}
       <Grid container spacing={2} style={{ backgroundColor:'white', color:'black' }}>
       {/* content */}
         <Grid xs={12} md={6} lg={6} xl={6} 
         
-        style={{ paddingLeft:50,paddingRight:10, paddingTop:50,paddingBottom:100, fontSize:10 }}>
+        style={{ paddingLeft:50,paddingRight:10, paddingTop:100,paddingBottom:100, fontSize:10 }}>
         <Typography variant="h3" gutterBottom>
          <b>Search Engine Optimization </b>
         </Typography>
@@ -88,12 +89,51 @@ export default function Home() {
 
         {/* image */}
         <Grid xs={12} md={6} lg={6} xl={6} style={{  paddingLeft:50 ,paddingTop:10 }}>
-        <img width="70%" src={banner1} alt="Reaact Image" />
+        <img width="70%" src={banner1} alt="React Image" />
         
           
         </Grid>
         
       </Grid>
+
+      {/* Grid row  */}
+      <Grid container spacing={2} style={{ backgroundColor:'#335b7c', color:'#FFFFFF' }}>
+      {/* content */}
+        <Grid xs={12} md={6} lg={6} xl={6} 
+        style={{  paddingLeft:50 ,paddingTop:20 }}
+          >
+          <img width="60%" src={banner2} alt="React Image" style={{ paddingTop:30, paddingLeft:50 }} />
+           
+        </Grid>
+
+        {/* image */}
+        <Grid xs={12} md={6} lg={6} xl={6} 
+        style={{ paddingLeft:50,paddingRight:10, paddingTop:100,paddingBottom:100, fontSize:10 }}
+        >
+        
+        <Typography variant="h3" gutterBottom>
+            <b>Search Engine Optimization </b>
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            </Typography>
+
+            {/* buttons */}
+            <Stack direction="row" spacing={2} mt={3}>
+              <Button variant="contained" style={{ backgroundColor: '#000000' }} endIcon={<SendIcon />}>
+                Read More
+              </Button>
+              
+            </Stack>
+       
+        
+          
+        </Grid>
+        
+      </Grid>
+
+      
     </Box>
 
     
