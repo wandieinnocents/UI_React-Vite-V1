cb23af5d701f28a00f325d073d9e190e5156f3a5
wandieinnocents <wandieinnocent2@gmail.com>
2023-05-27 12:53:32 +0300

    complete drawer and main nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import './App.css'
-import Header from './components/Header'
+// import Header from './components/Header'
+import Navbar from "./components/Navbar"
 import { Routes, Route } from 'react-router-dom'
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
 import Blog from './pages/Blog'
+import Contact from './pages/Contact'
+
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/products" element={<Products />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
