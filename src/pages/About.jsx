318a1be14114485514b8d9dd94a474b109966fc5
wandieinnocents <wandieinnocent2@gmail.com>
2023-05-27 11:49:28 +0300

    Fix menu

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+// import React from 'react';
 
 function About() {
   return (
     <div>
-     <h1>This is the about page</h1>
+     <h1>This is the about page aa</h1>
       {/* <Link to="/">Home</Link> <br></br> */}
       {/* <Link to="/products">Click to view our contact page</Link> */}
     </div>
