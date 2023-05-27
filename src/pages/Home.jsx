84f637867c4c5358f658edc14f6f445dd59742be
wandieinnocents <wandieinnocent2@gmail.com>
2023-05-27 16:50:19 +0300

    Add seo part

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,9 @@ import Stack from '@mui/material/Stack';
 
 // images
 import banner from "../assets/seo3.png"
+import banner1 from "../assets/seo.png"
+// import banner2 from "../assets/seo3.png"
+// import banner3 from "../assets/seo3.png"
 
 
 
@@ -23,6 +26,7 @@ import banner from "../assets/seo3.png"
 
 export default function Home() {
   return (
+    // landing view 1
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2} style={{ backgroundColor:'black', color:'white' }}>
       {/* content */}
@@ -50,13 +54,48 @@ export default function Home() {
         </Grid>
 
         {/* image */}
-        <Grid xs={12} md={6} lg={6} xl={6} style={{  paddingLeft:50 ,paddingTop:50 }}>
+        <Grid xs={12} md={6} lg={6} xl={6} style={{  paddingLeft:50 ,paddingTop:30 }}>
         <img width="70%" src={banner} alt="Reaact Image" />
         
           
         </Grid>
         
       </Grid>
+
+      {/* web development */}
+      <Grid container spacing={2} style={{ backgroundColor:'white', color:'black' }}>
+      {/* content */}
+        <Grid xs={12} md={6} lg={6} xl={6} 
+        
+        style={{ paddingLeft:50,paddingRight:10, paddingTop:120,paddingBottom:120, fontSize:10 }}>
+        <Typography variant="h3" gutterBottom>
+         <b>Search Engine Optimization </b>
+        </Typography>
+        <Typography variant="h5" gutterBottom>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        </Typography>
+
+        {/* buttons */}
+        <Stack direction="row" spacing={2} mt={3}>
+          <Button variant="contained" style={{ backgroundColor: '#f45e20' }} endIcon={<SendIcon />}>
+            Read More
+          </Button>
+          
+        </Stack>
+       
+        </Grid>
+
+        {/* image */}
+        <Grid xs={12} md={6} lg={6} xl={6} style={{  paddingLeft:50 ,paddingTop:30 }}>
+        <img width="70%" src={banner1} alt="Reaact Image" />
+        
+          
+        </Grid>
+        
+      </Grid>
     </Box>
+
+    
   );
 }
