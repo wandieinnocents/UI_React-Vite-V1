7b0210093b1207b5b5ff72adf9e738c99ddea290
wandieinnocents <wandieinnocent2@gmail.com>
2023-05-27 15:33:01 +0300

    Refactor code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@ import Box from '@mui/system/Box';
 import Grid from '@mui/system/Unstable_Grid';
 import styled from '@mui/system/styled';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
 
 
 const Item = styled('div')(({ theme }) => ({
@@ -20,14 +23,24 @@ export default function Home() {
       <Grid container spacing={2}>
       {/* content */}
         <Grid xs={12} md={6} lg={6} xl={6} 
-        style={{ paddingLeft:50,paddingRight:10, paddingTop:50, fontSize:10 }}>
+        style={{ paddingLeft:50,paddingRight:10, paddingTop:100, fontSize:10 }}>
         <Typography variant="h2" gutterBottom>
-         <b>Where Organic Intelligence Meets AI</b>
+         <b>Where <span style={{ color: '#f45e20' }}>Organic</span> Intelligence Meets AI</b>
         </Typography>
         <Typography variant="h5" gutterBottom>
         Linx Solutions understands the multiple components of a successful business. From developing a strong visual identity to 
         creating a brand awareness and a digital presence.
         </Typography>
+
+        {/* buttons */}
+        <Stack direction="row" spacing={2} mt={3}>
+          <Button variant="contained" color="secondary" endIcon={<SendIcon />}>
+            Learn More
+          </Button>
+          <Button variant="contained" endIcon={<SendIcon />}>
+            Contact Us
+          </Button>
+        </Stack>
        
         </Grid>
 
